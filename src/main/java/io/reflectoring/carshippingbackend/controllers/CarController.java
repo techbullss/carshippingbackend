8f043ea7c023d053df8ca42d8be86a7585a62cec
Maish <bwanmaina2010@gmail.com>
2025-08-18 12:04:42 +0300

    dddd

--- src/main/java/io/reflectoring/carshippingbackend/controllers/CarController.java
+++ src/main/java/io/reflectoring/carshippingbackend/controllers/CarController.java
@@ -17,7 +17,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin("http://localhost:3000/")
+@CrossOrigin("https://carshippingfrontend.vercel.app/")
 @RequestMapping("/api/cars")
 public class CarController {
     @Autowired

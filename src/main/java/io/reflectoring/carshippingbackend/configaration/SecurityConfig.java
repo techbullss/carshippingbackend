dbe814e1c4c6da2e3f77872360ed4525c158a365
Maish <bwanmaina2010@gmail.com>
2025-10-23 18:20:42 +0300

    done

--- src/main/java/io/reflectoring/carshippingbackend/configaration/SecurityConfig.java
+++ src/main/java/io/reflectoring/carshippingbackend/configaration/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("https://f-carshipping.com/"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST","PATCH", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 

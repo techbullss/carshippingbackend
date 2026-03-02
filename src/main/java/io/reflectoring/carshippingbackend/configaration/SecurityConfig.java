ef3fca713dda2a1c3e9829917a3dc333e5a9256e
Maish <bwanmaina2010@gmail.com>
2026-03-02 19:19:07 +0300

    fff

--- src/main/java/io/reflectoring/carshippingbackend/configaration/SecurityConfig.java
+++ src/main/java/io/reflectoring/carshippingbackend/configaration/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -27,6 +28,7 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 @Autowired
@@ -116,7 +118,6 @@ public class SecurityConfig {
 
                         // Admin endpoints (extra restricted)
                         // Allow GET requests to users endpoint
-                        .requestMatchers(HttpMethod.GET, "/api/admin/users", "/api/admin/users/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/motorcycles/dashboard/**").authenticated()
 
 
